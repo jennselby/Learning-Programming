24400b83d7bd58f3cf860f0d3289611932beaaa8
Jen Selby <jenselby@gmail.com>
2016-08-01 23:36:40 -0700

    Implement filtering

--- create_page.py
+++ create_page.py
@@ -4,14 +4,15 @@ import os.path
 
 CATEGORY_HTML = \
 '''
-        <div class="panel panel-primary">
-            <div class="category-item panel-heading">
-                <div class="panel-heading">
-                    <div class="category-name panel-title">{cat_name}</div>
-                    <div class="category-desc">{cat_desc}</div>
+            <div class="panel panel-primary">
+                <div class="category-item panel-heading">
+                    <div class="panel-heading">
+                        <div class="category-name panel-title">{cat_name}</div>
+                        <div class="category-desc">{cat_desc}</div>
+                    </div>
                 </div>
-            </div>
-            <div class="panel-body">
+                <div class="panel-body">
+                    <ul class="category-suggestions-list list-group">
 '''
 
 SUGGESTION_HTML = \
@@ -25,14 +26,35 @@ SUGGESTION_HTML = \
                     </li>
 '''
 
+FILTER_HEADER_HTML = \
+'''
+            <div class="panel panel-primary">
+                <div class="category-item panel-heading">
+                    <div class="panel-heading">
+                        <div class="filter-name panel-title">Filter by {filter_heading}</div>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <ul class="filter-list list-group" id="{filter_id}">
+'''
+
+FILTER_ITEM_HTML = \
+'''
+                        <li class="filter-item list-group-item showing" id="{filter_name}">{filter_name}</li>
+'''
+
 def create_html():
     indir = os.path.dirname(os.path.realpath(__file__))
 
     suggestions_html = ''
+    script = ''
+    uniq_languages = set()
+    uniq_subjects = set()
     with open(os.path.join(indir, 'suggestions.json'), 'rb') as indata:
         jsondata = json.loads(indata.read())
 
-        suggestions_html = '<div id="suggestions-list">\n'
+        suggestions_html = '<div class="row">\n\n'
+        suggestions_html = '    <div id="suggestions-list" class="col-md-8 col-md-pull-4">\n'
 
         for category in jsondata:
             cat_name, cat_desc, cat_suggestions = category
@@ -41,11 +63,11 @@ def create_html():
                                     cat_name=cat_name,
                                     cat_desc=cat_desc)
 
-            suggestions_html += \
-                '                <ul class="category-suggestions-list list-group">\n'
 
             for suggestion in cat_suggestions:
                 name, desc, url, levels, languages, subjects = suggestion
+                uniq_languages.update(languages.split())
+                uniq_subjects.update(subjects.split())
                 classes = ' '.join([levels, languages, subjects])
 
                 suggestions_html += SUGGESTION_HTML.format(
@@ -57,15 +79,52 @@ def create_html():
                                         languages=languages,
                                         subjects=subjects)
 
-            suggestions_html += '                </ul>\n'
-            suggestions_html += '            </div>\n'
-            suggestions_html += '        </div>\n\n'
+            suggestions_html += '                    </ul>\n'
+            suggestions_html += '                </div>\n'
+            suggestions_html += '            </div>\n\n'
 
-        suggestions_html += '    </div>\n'
+        suggestions_html += '        </div>\n'
+
+    filters_html = '    <div id="filters-list" class="col-md-4 col-md-push-8">\n'
+    script += '        var filters = {\n'
+    for filter_heading, filter_list in [
+            ('Difficulty Level', ['Beginner', 'Intermediate', 'Advanced']),
+            ('Subject', sorted(list(uniq_subjects))),
+            ('Programming Language', sorted(list(uniq_languages)))]:
+        filter_id = filter_heading.replace(' ', '')
+        filters_html += FILTER_HEADER_HTML.format(
+                            filter_heading=filter_heading,
+                            filter_id=filter_id)
+
+        script += '            "{filter_id}": {filter_list},\n'.format(
+                    filter_id=filter_id,
+                    filter_list=[str(x) for x in filter_list])
+
+        for filter_name in filter_list:
+            filters_html += FILTER_ITEM_HTML.format(filter_name=filter_name)
+
+        filters_html += '                </ul>\n'
+        filters_html += '            </div>\n\n'
+        filters_html += '        </div>\n\n'
+
+    filters_html += '        </div>\n\n'
+    filters_html += '    </div>\n\n'
+
+    script += '        };\n\n'
+
+    style = ''
+    with open(os.path.join(indir, 'suggestions.css'), 'rb') as instyle:
+        style = instyle.read()
+
+    with open(os.path.join(indir, 'suggestions.js'), 'rb') as inscript:
+        script += inscript.read()
 
     html = ''
     with open(os.path.join(indir, 'template.html'), 'rb') as inhtml:
-        html = inhtml.read().format(suggestions=suggestions_html)
+        html = inhtml.read().format(suggestions=suggestions_html,
+                                    filters=filters_html,
+                                    style=style,
+                                    script=script)
 
     return html
 
--- suggestions.js
+++ suggestions.js
@@ -1,13 +1,99 @@
-function filterSuggestions(elem) {
-    
-}
+        function getCategory(filterItem) {
+            var parentElem = filterItem;
+            do {
+                parentElem = parentElem.parentElement;
+            }
+            while (parentElem.nodeName !== 'UL');
 
-var filters = document.getElementsByClass('filter');
+            return parentElem.id;
+        }
 
-for (var index = 0; index < filters.length; ++index) {
-    var filter = filters[index];
+        function checkSuggestionVisibility(suggestion) {
+            var visible = false;
+            filterCategories = Object.keys(filters);
+            for (var catIndex = 0;
+                 catIndex < filterCategories.length;
+                 ++catIndex) {
+                visible = false;
 
-    filter.addEventListener('click', function (event) {
-        filterSuggestions(event.target);
-    });
-}
+                var filterNames = filters[filterCategories[catIndex]];
+                for (var nameIndex = 0;
+                     nameIndex < filterNames.length;
+                     ++nameIndex) {
+                    var filterName = filterNames[nameIndex];
+                    if (suggestion.classList.contains(filterName)) {
+                        visible = true;
+                    }
+                }
+
+                // If any one category has no items that are in the
+                // class list, the item should not be visible. Stop
+                // checking.
+                if (visible === false) {
+                    suggestion.style.display = 'none';
+                    break;
+                }
+            }
+
+            if (visible === true) {
+                suggestion.style.display = 'block';
+            }
+        }
+
+        function refilter() {
+            var suggestions = document.getElementsByClassName(
+                'suggestion-item');
+
+            for (var suggIndex = 0;
+                 suggIndex < suggestions.length;
+                 ++suggIndex) {
+                var suggestion = suggestions[suggIndex];
+                checkSuggestionVisibility(suggestion);
+            }
+        }
+
+        function toggleFilter(filterItem) {
+            if (filterItem.classList.contains('filtering')) {
+                // switch to showing
+
+                filterItem.classList.add('showing');
+                filterItem.classList.remove('filtering');
+
+                // add this to the list of things that should be displayed
+                var category = getCategory(filterItem);
+                var filterName = filterItem.id;
+                filters[category].push(filterName);
+
+                // change what items are displayed
+                refilter();
+
+            } else {
+                // switch to filtering
+                // showing is the default; if there was a mistake and
+                // it has neither class, we can ignore that error
+
+                filterItem.classList.add('filtering');
+                filterItem.classList.remove('showing');
+
+                // remove this from the list of things that should
+                // be displayed
+                var category = getCategory(filterItem);
+                var categoryFilters = filters[category];
+                var filterName = filterItem.id;
+                var filterIndex = categoryFilters.indexOf(filterName);
+                while (filterIndex !== -1) {
+                    categoryFilters.splice(filterIndex, 1);
+                    filterIndex = categoryFilters.indexOf(filterName);
+                }
+
+                // change what items are displayed
+                refilter();
+            }
+        }
+
+        var filterItems = document.getElementsByClassName('filter-item');
+        for (var index = 0; index < filterItems.length; ++index) {
+            filterItems[index].addEventListener('click', function(event) {
+                toggleFilter(event.target);
+            });
+        }
